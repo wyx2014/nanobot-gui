5fe1f723c0b5608f84e9f10a51c3f3c42106cabf
tree <403145109@qq.com>
2026-06-10 11:13:13 +0800

    feat: init

--- test-serialize.ts
+++
@@ -1,7 +0,0 @@
-import { serializeExpertMd } from './src/core/expert/registry';
-try {
-  const md = serializeExpertMd({ name: 'test', description: 'test desc' }, 'hello world');
-  console.log('SUCCESS:', JSON.stringify(md));
-} catch (err) {
-  console.error('ERROR:', err);
-}
